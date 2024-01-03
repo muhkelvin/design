f03b8a099e20c0f8dcdee92f10d0a96b5f808b1a
MuhKelvin <muhkelvin36@gmail.com>
2024-01-03 20:47:42 +0700

    l

---
+++ app/src/main/java/com/example/desgignchapter/register/DetailRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desgignchapter.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desgignchapter.R
+
+class DetailRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_register)
+    }
+}
